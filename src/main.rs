f14d70cc327342aaad6297db36ef24b7a9277050
unknown <almiejanecampion@gmail.com>
2023-07-12 02:56:41 +0800

    update bevy_mod_picking

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use bevy::prelude::*;
+use bevy_mod_picking::*;
+
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .run();
-}
-
+    //Window Setup
+      .add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+          resolution: (640.0, 400.0).into(),
+          title: "Project Tracking".to_string(),
+          ..default()
+        }),
+        ..default()
+      }))
+      //Mod Picking
+      .add_plugins(DefaultPickingPlugins)
+      .add_startup_system(setup)
+      .run();
+  }
 
 /// set up a simple scene with a "parent" cube and a "child" cube
 fn setup(
@@ -14,9 +25,16 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let cube_handle = meshes.add(Mesh::from(shape::Cube { size: 2.0 }));
+    let cube_handle = meshes.add(Mesh::from(shape::Cube { size: 1.0 }));
     let cube_material_handle = materials.add(StandardMaterial {
         base_color: Color::rgb(0.8, 0.7, 0.6),
+        ..default()
+    });
+
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(10.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
 
@@ -26,7 +44,7 @@ fn setup(
             PbrBundle {
                 mesh: cube_handle.clone(),
                 material: cube_material_handle.clone(),
-                transform: Transform::from_xyz(0.0, 0.0, 1.0),
+                transform: Transform::from_xyz(0.0, 0.5, 0.0),
                 ..default()
             },
         ))
