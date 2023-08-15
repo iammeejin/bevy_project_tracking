20b34c0d6e2240b46f05aa35f0615a46304c73ac
unknown <almiejanecampion@gmail.com>
2023-08-16 02:34:31 +0800

    Implemented new feature for Project 2 Button

--- src/project_list/ui/interactions.rs
+++ src/project_list/ui/interactions.rs
@@ -1,7 +1,6 @@
 use bevy::prelude::*;
 use bevy::app::AppExit;
 
-use crate::AppState;
 use crate::project_list::components::*;
 use crate::project_list::styles::*;
 
@@ -19,26 +18,21 @@ pub fn interact_with_project_1_button(
     }
 }
 
+
+
 pub fn interact_with_project_2_button(
-    mut button_query: Query<(&Interaction, &mut BackgroundColor),(Changed<Interaction>, With<Project2Button>),
+    mut button_query: Query<(&Interaction, &mut BackgroundColor,Option<&SelectedOption>),(Changed<Interaction>, With<Project2Button>),
     >,
-    mut app_state_next_state: ResMut<NextState<AppState>>,
 ) {
-    if let Ok((interaction, mut background_color)) = button_query.get_single_mut() {
-        match *interaction {
-            Interaction::Clicked => {
-                *background_color = PRESSED_BUTTON_COLOR.into();
-                app_state_next_state.set(AppState::MainMenu);
-            }
-            Interaction::Hovered => {
-                *background_color = HOVERED_BUTTON_COLOR.into();
-            }
-            Interaction::None => {
-                *background_color = NORMAL_BUTTON_COLOR.into();
+    for (interaction, mut color, selected) in &mut button_query {
+        *color = match (*interaction, selected) {
+            (Interaction::Clicked, _) | (Interaction::None, Some(_)) => PRESSED_BUTTON_COLOR.into(),
+            (Interaction::Hovered, Some(_)) => HOVERED_PRESSED_BUTTON_COLOR.into(),
+            (Interaction::Hovered, None) => HOVERED_BUTTON_COLOR.into(),
+            (Interaction::None, None) => NORMAL_BUTTON_COLOR.into(),
         }
     }
-}}
-
+}
 
 pub fn interact_with_quit_button(
     mut app_exit_event_writer: EventWriter<AppExit>,
